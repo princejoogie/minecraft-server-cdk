4050bcd1c91fbf1530a7d77874d2b36031b352f4
princejoogie <princejoogie@gmail.com>
2022-12-08 19:32:24 +0800

    chore: add env
    new file:   .env.example
    modified:   .gitignore
    modified:   bin/minecraft-server-cdk.ts
    modified:   lib/minecraft-server-cdk-stack.ts
    modified:   package.json
    modified:   yarn.lock

--- bin/minecraft-server-cdk.ts
+++ bin/minecraft-server-cdk.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+
+import "dotenv/config";
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { MinecraftServerCdkStack } from "../lib/minecraft-server-cdk-stack";
@@ -10,9 +12,12 @@ new MinecraftServerCdkStack(app, "MinecraftServerCdkStack", {
    * but a single synthesized template can be deployed anywhere. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: "123456789012", region: "us-east-1" },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
