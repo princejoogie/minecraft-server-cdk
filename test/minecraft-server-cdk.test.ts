762b2e8bc1e9d42af51c8187ca767c7e0ada7751
princejoogie <princejoogie@gmail.com>
2022-12-08 19:17:03 +0800

    Initial commit

---
+++ test/minecraft-server-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MinecraftServerCdk from '../lib/minecraft-server-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/minecraft-server-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MinecraftServerCdk.MinecraftServerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
